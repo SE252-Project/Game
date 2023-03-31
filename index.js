0eaf1b791c18b807d148a6a30e072643633b2b76
FengD020508 <1010764219@qq.com>
2023-03-31 09:48:16 +0800

    Start buttton

--- index.js
+++
@@ -1,5 +0,0 @@
-// script.js
-document.getElementById("start-button").addEventListener("click", function() {
-    // Redirect to game page or load game resources here
-    window.location.href = "./game.html";
-  });
